c8fccc84645000223143517dbd5cef30e030d2d2
LewJ <lew@lewj.me>
2018-05-31 14:07:49 +0100

    option to use dark theme

--- lib/material_search.dart
+++ lib/material_search.dart
@@ -49,6 +49,7 @@ class MaterialSearch<T> extends StatefulWidget {
     this.barBackgroundColor = Colors.white,
     this.iconColor = Colors.black,
     this.leading,
+    this.brightness = Brightness.light,
   }) : assert(() {
          if (results == null && getResults == null
              || results != null && getResults != null) {
@@ -71,6 +72,7 @@ class MaterialSearch<T> extends StatefulWidget {
   final Color barBackgroundColor;
   final Color iconColor;
   final Widget leading;
+  final Brightness brightness;
 
   @override
   _MaterialSearchState<T> createState() => new _MaterialSearchState<T>();
@@ -178,7 +180,7 @@ class _MaterialSearchState<T> extends State<MaterialSearch> {
       appBar: new AppBar(
         leading: widget.leading,
         iconTheme: iconTheme,
-        brightness: Brightness.dark,
+        brightness: brightness,
         title: new TextField(
           controller: _controller,
           autofocus: true,
