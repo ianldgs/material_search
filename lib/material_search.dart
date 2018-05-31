fbbb321602b3cc07fe54e6aa2dc07842a13df072
LewJ <lew@lewj.me>
2018-05-31 14:18:29 +0100

    I'm an idiot, leave me alone

--- lib/material_search.dart
+++ lib/material_search.dart
@@ -49,7 +49,7 @@ class MaterialSearch<T> extends StatefulWidget {
     this.barBackgroundColor = Colors.white,
     this.iconColor = Colors.black,
     this.leading,
-    this.appbarBrightness = Brightness.light,
+    this.brightness = Brightness.light,
   }) : assert(() {
          if (results == null && getResults == null
              || results != null && getResults != null) {
@@ -72,7 +72,7 @@ class MaterialSearch<T> extends StatefulWidget {
   final Color barBackgroundColor;
   final Color iconColor;
   final Widget leading;
-  final Brightness appbarBrightness;
+  final Brightness brightness;
 
   @override
   _MaterialSearchState<T> createState() => new _MaterialSearchState<T>();
@@ -180,7 +180,7 @@ class _MaterialSearchState<T> extends State<MaterialSearch> {
       appBar: new AppBar(
         leading: widget.leading,
         iconTheme: iconTheme,
-        brightness: appbarBrightness,
+        brightness: widget.brightness,
         title: new TextField(
           controller: _controller,
           autofocus: true,
